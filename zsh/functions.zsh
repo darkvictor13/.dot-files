05904402359f3b80615667bc03b1188b7c8b1c8a
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2020-09-15 16:16:00 -0300

    Adicionando func ex

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -7,8 +7,36 @@ function auto_git() {
 
 # funcao compilando
 function compile() {
-  gcc $1.c -o $1 -lm
+  case $1 in
+    *.c)      gcc $1 -o exec -lm  ;;
+    *.cpp)    g++ $1 -o exec      ;;
+    #*.asm)    "nasm -f elf64 $1 && ld  ";;
+  esac
 }
 function compile_run() {
   gcc $1.c -o $1 -lm && ./$1
+}
+
+ex () {
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1   ;;
+      *.tar.gz)    tar xzf $1   ;;
+      *.bz2)       bunzip2 $1   ;;
+      *.rar)       unrar x $1   ;;
+      *.gz)        gunzip $1    ;;
+      *.tar)       tar xf $1    ;;
+      *.tbz2)      tar xjf $1   ;;
+      *.tgz)       tar xzf $1   ;;
+      *.zip)       unzip $1     ;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1      ;;
+      *.deb)       ar x $1      ;;
+      *.tar.xz)    tar xf $1    ;;
+      *.tar.zst)   unzstd $1    ;;      
+      *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
 }
