e33e9ed3cd99c79300275248b1dbba52889cee65
darkvictor13 <01victoralmeida@gmail.com>
2021-02-02 13:45:50 -0300

    adicionando novos snippets

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,7 +1,7 @@
 # apos dar cd ele da ls automaticamente
 chpwd() ls
 
-# faz o terminal se esconde 
+# faz o terminal se esconder
 alias zathura="devour zathura"
 alias vlc="devour vlc"
 alias cvlc="devour cvlc"
@@ -13,16 +13,31 @@ alias neofetch='figlet -oc "ArchLinux"| lolcat && figlet -oc "DoVictor"| lolcat 
 alias vifm="~/.config/vifm/scripts/vifmrun"
 # Disponibliza a temperatura da minha cidade no terminal
 alias temp="curl wttr.in"
-# faz o mv perguntar antes de sobreescrever os aqruivos
-alias mv="mv -i"
+# perguntar antes de sobreescrever os aqruivos
+alias mv="mv -iv"
+alias cp="cp -iv"
+# mais informações antes de remover arquivos
+alias rm="rm -v"
+# Roda comando telas sem precisar de sudo
+alias telas="sudo /home/victor/Repos/scripts/telas"
 
 # acessando pastas rapidamente
+# 2 ano
 alias clp="cd ~/Docs/facul/2-ano/CLP"
 alias so="cd ~/Docs/facul/2-ano/SO"
 alias ies="cd ~/Docs/facul/2-ano/IES"
 alias lfa="cd ~/Docs/facul/2-ano/LFA"
 alias lm="cd ~/Docs/facul/2-ano/LM"
 alias trc="cd ~/Docs/facul/2-ano/TRC"
+
+# 3 ano
+alias aed="cd ~/Docs/facul/3-ano/AED"
+alias c2="cd ~/Docs/facul/3-ano/C2"
+alias cn="cd ~/Docs/facul/3-ano/CN"
+alias e="cd ~/Docs/facul/3-ano/Estagio"
+alias oac="cd ~/Docs/facul/3-ano/OAC"
+alias paa="cd ~/Docs/facul/3-ano/PAA"
+alias sd="cd ~/Docs/facul/3-ano/SD"
 
 alias gr="~/Docs/Jesus/Gr"
 alias preg="~/Docs/Jesus/Gr/Minhas-palavras"
